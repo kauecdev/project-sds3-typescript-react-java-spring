32c0aa2f0fb4c5b1b8bcfd7853931ee168a9d5a7
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-05-03 07:48:41 -0300

    Project created

---
+++ backend/src/test/java/com/project/sds3/Projeto/SDS3/ProjetoSds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sds3.Projeto.SDS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
